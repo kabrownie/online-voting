cbe97fb059017656f615e060798a6025c45ce797
Ka-ra-nja <makonokevin@gmail.com>
2022-03-18 15:22:36 +0300

    -

--- src/voting.java
+++ src/voting.java
@@ -1,2 +1,21 @@
-public class voting {
+import javax.swing.*;
+
+public class voting  extends JFrame {
+    private JPanel votingpanel;
+
+    public voting(){
+
+        setContentPane(votingpanel);
+        setTitle("welcome");
+        setSize(450, 400);
+        setResizable(false);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+
+    }
+    public static void main(String[]args){
+        new voting();
+    }
 }
+
